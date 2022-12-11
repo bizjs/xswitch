6260958cfa14c3677b389e80aa409d8ce5ed3ae2
hstarorg <hm910705@163.com>
2022-12-11 15:13:40 +0800

    chore: upgrade manifest to v3, now not working

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
-import { stripJsonComments } from './strip-json-comments';
-import { REG, EMPTY_STRING } from './constants';
+import { stripJsonComments } from "./strip-json-comments";
+import { REG, EMPTY_STRING } from "./constants";
 
 export function JSONC2JSON(jsonc: string): string {
   return stripJsonComments(jsonc)
@@ -7,10 +7,29 @@ export function JSONC2JSON(jsonc: string): string {
     .replace(REG.TRIM_JSON, ($0, $1, $2) => $2);
 }
 
-export function JSON_Parse(json: string, cb: (error: object | boolean, json?: object) => void): void {
+export function JSON_Parse(
+  json: string,
+  cb: (error: object | boolean, json?: object) => void
+): void {
   try {
     cb(false, JSON.parse(json));
   } catch (e) {
     cb(e);
+  }
+}
+
+/**
+ * 安全的 Parse JSON 不报错
+ * @param jsonStr
+ * @returns
+ */
+export function safeJSONParse<T = any>(
+  jsonStr: string
+): { success: true; value: T } | { success: false; error: any } {
+  try {
+    const value = JSON.parse(jsonStr);
+    return { success: true, value };
+  } catch (e) {
+    return { success: false, error: e };
   }
 }
